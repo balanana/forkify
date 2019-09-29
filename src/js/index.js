53227c0e4dfb72d4ba6a5c5dd3607adc14a8b4fa
balanana <balan.ana.v@gmail.com>
2019-09-29 12:58:49 +0300

    Start Recipe Controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,66 @@
+import Search from './models/Search';
+import Recipe from './models/Recipe';
+import * as searchView from './views/searchView'
+import { elements, renderLoader, clearLoader } from './views/base';
 
-import axios from 'axios';
 
-async function getResults(query){
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-    const key = 'aa9ed1b747c75daa4aab10e48fd4e0f0';
-   const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-    console.log
+/** Global state of the map
+ * Current recipe object
+ * Shopping list object
+ * Liked Recipes
+ */
+const state ={};
+
+
+/**
+ * SEARCH CONTROLLER
+ */
+
+const controlSearch = async () => {
+    // 1) get the query from view
+    const query = searchView.getInput(); 
+    //console.log(query);
+
+    if (query){
+        //2) new search object and to state
+        state.search = new Search(query);
+
+        //3) Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
+
+        //4) Search for recipes
+        await state.search.getResults();
+
+        //5) Render results on UI
+        clearLoader();
+        searchView.renderResults(state.search.result);
+    }
+
 }
 
-getResults('pizza');
-// 
-//https://www.food2fork.com/api/search
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    if (btn){
+        const goToPage = parseInt(btn.dataset.goto,10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+    }
+});
+
+/**
+ * RECIPE CONTROLLER
+ */
+const controlRecipe = () => {
+    const id = window.location.hash;
+    console.log(id);
+}
+
+window.addEventListener('hasgchange',controlRecipe);
